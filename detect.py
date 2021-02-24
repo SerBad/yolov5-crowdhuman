719dae396972d2801d46a36d0943f101e2a3da19
deepakcrk <deepakcrk@gmail.com>
2021-02-24 22:49:02 +0530

    --heads

--- detect.py
+++ detect.py
@@ -110,8 +110,10 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
-                        if opt.person:
-                            if 'person' in label:
+                        if opt.heads or opt.person:
+                            if 'head' in label and opt.heads:
+                                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                            if 'person' in label and opt.person:
                                 plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
                         else:
                             plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
@@ -167,6 +169,7 @@ if __name__ == '__main__':
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--person', action='store_true', help='displays only person')
+    parser.add_argument('--heads', action='store_true', help='displays only person')
     opt = parser.parse_args()
     print(opt)
     check_requirements()
